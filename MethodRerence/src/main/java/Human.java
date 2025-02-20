9f4d6bc2aaed3690638b6d9a11927653dbdf601e
Ling0 <1355733858@qq.com>
2025-02-20 20:17:47 +0800

    2025/2/20 方法引用整理完成

---
+++ MethodRerence/src/main/java/Human.java
@@ -0,0 +1,6 @@
+public class Human {
+
+    public void say(){
+        System.out.println("Hello, I am a human!");
+    }
+}
---
+++ MethodRerence/src/main/java/SuperAndThisMethodReference.java
@@ -0,0 +1,16 @@
+public class SuperAndThisMethodReference {
+
+    public static void main(String[] args) {
+        /*
+        * 父方法的引用
+        * 语法：
+        *   super::方法名
+        *
+        * this方法的引用
+        * 语法：
+        *   this::方法名
+        * */
+
+
+    }
+}
